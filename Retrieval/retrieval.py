5c528979b7c1b1db6d7f3e1e2bed38169ac17644
Heical Chandra <haical31@gmail.com>
2025-03-29 19:23:13 +0700

    Update retrieval.py

--- Retrieval/retrieval.py
+++ Retrieval/retrieval.py
@@ -1,6 +1,7 @@
 import chromadb
 import numpy as np
 from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
 
 class Retrieval:
     def __init__(self, db_path="../Database"):
@@ -9,19 +10,46 @@ class Retrieval:
         self.chroma_client = chromadb.PersistentClient(path=self.db_path)
         self.collection = self.chroma_client.get_or_create_collection(name="nutrition")
     
-    def retrieve_documents(self, query, top_k=1):
+    def retrieve_documents(self, query, top_k=5):
         query_embedding = self.embedding_model.encode([query]).tolist()[0]
         results = self.collection.query(query_embeddings=[query_embedding], n_results=top_k)
-        
+
         retrieved_docs = []
-        for doc_id, metadata in zip(results["ids"], results["metadatas"]):
-            retrieved_docs.append(metadata)
-        
+
+        for metadata_list in results["metadatas"]:  
+            if metadata_list:  
+                retrieved_docs.extend(metadata_list)
+
         return retrieved_docs
+
+    
+    def sentence_similarity(self, query, retrieved_docs):
+        query_embedding = self.embedding_model.encode([query])
+        resource_embeddings = self.embedding_model.encode([r['name'] for r in retrieved_docs])
+        
+        similarities = cosine_similarity(query_embedding, resource_embeddings)[0]
+        best_match_idx = np.argmax(similarities)
+        
+        return retrieved_docs[best_match_idx]
 
 if __name__ == "__main__":
     rag = Retrieval()
     query = "ayam goreng"
-    results = rag.retrieve_documents(query)
-    for result in results:
-        print(result)
+    
+    # Get top 5 relevant documents
+    retrieved_docs = rag.retrieve_documents(query, top_k=5)
+    
+    # Find the most similar document
+    best_match = rag.sentence_similarity(query, retrieved_docs)
+    
+    print("Best Match:", best_match)
+    print("--"*50)
+    print("perbandingan :", retrieved_docs)
+
+
+# if __name__ == "__main__":
+#     rag = Retrieval()
+#     query = "ayam goreng"
+#     results = rag.retrieve_documents(query)
+#     for result in results:
+#         print(result)
